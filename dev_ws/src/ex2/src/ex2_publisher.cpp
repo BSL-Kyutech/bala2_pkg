604b1e8d421316957428d67ea59a99a72cb9acf8
izumi0x01 <izumisoh.develop@gmail.com>
2024-09-08 23:22:05 +0900

    [add]Char型でa...zまでをpublish

--- dev_ws/src/ex2/src/ex2_publisher.cpp
+++ dev_ws/src/ex2/src/ex2_publisher.cpp
@@ -18,7 +18,7 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/char.hpp"
 
 using namespace std::chrono_literals;
 
@@ -31,7 +31,7 @@ public:
   MinimalPublisher()
       : Node("minimal_publisher"), count_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("ex2", 10);
+    publisher_ = this->create_publisher<std_msgs::msg::Char>("ex2", 10);
     timer_ = this->create_wall_timer(
         500ms, std::bind(&MinimalPublisher::timer_callback, this));
   }
@@ -39,13 +39,14 @@ public:
 private:
   void timer_callback()
   {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    auto message = std_msgs::msg::Char();
+    message.data = 'a' + (count_ % 26);
+    count_++;
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%c'", message.data);
     publisher_->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Publisher<std_msgs::msg::Char>::SharedPtr publisher_;
   size_t count_;
 };
 
