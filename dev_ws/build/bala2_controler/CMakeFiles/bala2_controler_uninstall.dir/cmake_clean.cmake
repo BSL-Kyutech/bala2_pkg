9e6bd89141474e4e24c7fcc6af34a2a0b02244f0
izumi0x01 <izumisoh.develop@gmail.com>
2024-09-08 23:54:59 +0900

    [delete]いらないものを消去

--- dev_ws/build/bala2_controler/CMakeFiles/bala2_controler_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bala2_controler_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bala2_controler_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
